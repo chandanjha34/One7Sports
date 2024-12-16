46fe6030e9d35c7c944ec28356bec0e8888543a8
jhachanda <jha95375@gmail.com>
2024-12-17 03:18:03 +0530

    Wesbite Sone

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,34 +1,72 @@
-import React from 'react'
-import '../sportsManagement/Sports.css'
-import tick from '../../assets/AboutPage_assets/tick.png'
-function Offers(){
+import React from 'react';
+import '../sportsManagement/Sports.css';
+import image1 from '../../assets/Franchise_assets/image1.png';
+import image2 from '../../assets/Franchise_assets/image2.png';
+import image3 from '../../assets/Franchise_assets/image3.png';
+import image4 from '../../assets/Franchise_assets/image4.png';
+import tick from '../../assets/AboutPage_assets/tick.png';
 
-    const offers = [
-        { id: 1, league:"Comprehensive Sporting Ecosystem",description:"By integrating venues, leagues, and technology, One7 Sports offers a holistic approach to sports development and player engagement."},
-        { id: 2, league:"Player Centric Leagues", description:"Our leagues are designed with a unique focus on different player demographics—corporate professionals, seasoned athletes, and aspiring youth"},
-        { id: 3, league:"Technological Innovation",description:"Leveraging technology to deliver real-time data, analytics, and player feedback, making training and performance optimization seamless and impactful."},
-        { id: 4, league:"Franchise-Based Model",description:"All leagues are franchise-based, offering exciting opportunities for player auctions and fostering healthy competition, strategic team-building, and robust fan engagement."},
-      ];
+function Offers() {
+  const offers = [
+    {
+      id: 1,
+      image: image1,
+      league: 'Comprehensive Sporting Ecosystem',
+      description:
+        'By integrating venues, leagues, and technology, One7 Sports offers a holistic approach to sports development and player engagement.',
+    },
+    {
+      id: 2,
+      image: image2,
+      league: 'Player Centric Leagues',
+      description:
+        'Our leagues are designed with a unique focus on different player demographics—corporate professionals, seasoned athletes, and aspiring youth.',
+    },
+    {
+      id: 3,
+      image: image3,
+      league: 'Technological Innovation',
+      description:
+        'Leveraging technology to deliver real-time data, analytics, and player feedback, making training and performance optimization seamless and impactful.',
+    },
+    {
+      id: 4,
+      image: image4,
+      league: 'Franchise-Based Model',
+      description:
+        'All leagues are franchise-based, offering exciting opportunities for player auctions and fostering healthy competition, strategic team-building, and robust fan engagement.',
+    },
+  ];
 
   return (
     <div>
-        <div className="text-center text-2xl font-bold text-[#F59431] pt-[4rem]">Top Leagues</div>
-          <div className="flex flex-col items-center md:flex-row md:justify-around px-[1rem] py-[3rem] gap-[3rem]">
-            <div className="card-container flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-8">
-              {offers.map((offers) => (
-                <div className="card" key={offers.id}>
-                  <div className="card-front">
-                    <img src={tick} alt="Trophy Icon" className="w-[3rem]" />
-                    <h3 className='text-xl text-white'>{offers.league}</h3>
-                  </div>
-                  <div className="card-back">
-                    <p>{offers.description}</p>
-                  </div>
-                </div>
-              ))}
+      <div className="text-center text-2xl font-bold text-[#F59431] pt-[4rem]">
+        Our Unique Offering
+      </div>
+      <div className="flex flex-col items-center md:flex-row md:justify-around px-[1rem] py-[3rem] gap-[3rem]">
+        <div className="card-container flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-8">
+          {offers.map((offer) => (
+            <div
+              key={offer.id}
+              className="card"
+              style={{
+                backgroundImage: `url(${offer.image})`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+              }}
+            >
+              <div className="card-front">
+                <p> </p>
+              </div>
+              <div className="card-back">
+                <p>{offer.description}</p>
+              </div>
             </div>
-          </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
+
 export default Offers;
--- src/components/sportsManagement/Sports.jsx
+++ src/components/sportsManagement/Sports.jsx
@@ -1,36 +1,70 @@
-import React from 'react'
-import trophy from '../../assets/AboutPage_assets/trophy.png'
-import card from '../../assets/AboutPage_assets/card.png'
-import './Sports.css'
+import React from 'react';
+import trophy from '../../assets/AboutPage_assets/trophy.png';
+import image1 from '../../assets/Franchise_assets/image1.png';
+import image2 from '../../assets/Franchise_assets/image2.png';
+import image3 from '../../assets/Franchise_assets/image3.png';
+import image4 from '../../assets/Franchise_assets/image4.png';
+import './Sports.css';
 
-function Sports(){
-
+function Sports() {
   const sports = [
-    { id: 1, league:"One7 Big Boys League (BBL)",description:"Designed exclusively for corporate professionals aged 30+ who retain their love, passion, and skills for cricket The league offers a platform for corporate leaders to showcase their talent on the field and connect with like-minded professionals."},
-    { id: 2, league:"One7 Big Boys League Pro (BBL Pro)",description:"Tailored for professional players aged 30+ seeking competitive cricket at the highest level."},
-    { id: 3, league:"One7 North India Pro Cricket League (NICPL)",description:"Targets young cricketers from across 7 states in North India. Provides a unique platform for talent development and exposure through franchise-based cricket leagues."},
-    { id: 4, league:"One7 Women Premier League",description:"Focused on nurturing and developing women cricketers across North India. Aims to provide visibility and growth opportunities through franchise-based competitions and auctions."},
+    {
+      id: 1,
+      image: image1,
+      league: 'One7 Big Boys League (BBL)',
+      description:
+        'Designed exclusively for corporate professionals aged 30+ who retain their love, passion, and skills for cricket. The league offers a platform for corporate leaders to showcase their talent on the field and connect with like-minded professionals.',
+    },
+    {
+      id: 2,
+      image: image2,
+      league: 'One7 Big Boys League Pro (BBL Pro)',
+      description:
+        'Tailored for professional players aged 30+ seeking competitive cricket at the highest level.',
+    },
+    {
+      id: 3,
+      image: image3,
+      league: 'One7 North India Pro Cricket League (NICPL)',
+      description:
+        'Targets young cricketers from across 7 states in North India. Provides a unique platform for talent development and exposure through franchise-based cricket leagues.',
+    },
+    {
+      id: 4,
+      image: image4,
+      league: 'One7 Women Premier League',
+      description:
+        'Focused on nurturing and developing women cricketers across North India. Aims to provide visibility and growth opportunities through franchise-based competitions and auctions.',
+    },
   ];
 
   return (
-        <div className="containers">
-          <div className="text-center text-2xl font-bold text-[#F59431] pt-[4rem]">Top Leagues</div>
-          <div className="flex flex-col items-center md:flex-row md:justify-around px-[1rem] pb-[3rem] gap-[3rem]">
-            <div className="card-container flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-8">
-              {sports.map((sports) => (
-                <div className="card" key={sports.id}>
-                  <div className="card-front">
-                    <img src={trophy} alt="Trophy Icon" className="icon" />
-                    <h3 className='text-xl text-white'>{sports.league}</h3>
-                  </div>
-                  <div className="card-back">
-                    <p>{sports.description}</p>
-                  </div>
-                </div>
-              ))}
+    <div className="containers">
+      <div className="text-center text-2xl font-bold text-[#F59431] pt-[4rem]">
+        Top Leagues
+      </div>
+      <div className="flex flex-col items-center md:flex-row md:justify-around px-[1rem] pb-[3rem] gap-[3rem]">
+        <div className="card-container flex flex-col sm:flex-row sm:flex-wrap items-center justify-center gap-8">
+          {sports.map((sport) => (
+            <div
+              key={sport.id}
+              className="card relative bg-cover bg-center text-white"
+              style={{
+                backgroundImage: `url(${sport.image})`,
+              }}
+            >
+              <div className="card-front bg-black bg-opacity-50 p-4 rounded-lg flex flex-col items-center">
+                <p></p>
+              </div>
+              <div className="card-back bg-white p-4 rounded-lg text-black">
+                <p>{sport.description}</p>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
+      </div>
+    </div>
   );
 }
+
 export default Sports;
